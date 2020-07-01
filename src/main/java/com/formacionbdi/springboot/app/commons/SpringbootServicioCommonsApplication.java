1c5ab0efaf73f8197603367732a5c28d170bab95
jlferrete <joseluisferrete@gmail.com>
2020-07-01 18:11:35 +0200

    Created commons library and generating jar using maven

---
+++ src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,10 @@
+package com.formacionbdi.springboot.app.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+	
+
+}
